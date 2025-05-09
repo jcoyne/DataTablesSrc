bd108e8aa8c62a3b32e5387e63ebc91103984b0b
Justin Coyne <jcoyne@justincoyne.com>
2025-05-09 11:09:33 -0500

    Fix link to documentation for bootstrap5

--- js/integration/dataTables.bootstrap5.js
+++ js/integration/dataTables.bootstrap5.js
@@ -6,7 +6,7 @@
  * DataTables integration for Bootstrap 5.
  *
  * This file sets the defaults and adds options to DataTables to style its
- * controls using Bootstrap. See https://datatables.net/manual/styling/bootstrap
+ * controls using Bootstrap. See https://datatables.net/manual/styling/bootstrap5
  * for further information.
  */
 
